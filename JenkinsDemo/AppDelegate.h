d7ae007835abd20d4e3801f007988ad82e287578
CaoYutian <1813739123@qq.com>
2017-04-25 14:48:49 +0800

    JenkinsDemo测试
    JenkinsDemo测试！！！

---
+++ JenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsDemo
+//
+//  Created by 。。。 on 2017/4/25.
+//  Copyright © 2017年 isofoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsDemo
+//
+//  Created by 。。。 on 2017/4/25.
+//  Copyright © 2017年 isofoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
